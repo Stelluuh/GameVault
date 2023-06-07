210e9524183842f4e64c6780711784e2b437d798
stella <sftamesis13@gmail.com>
2023-06-07 13:42:26 -0700

    Perform migration

--- db/migrate/20230607204102_create_statistics.rb
+++ db/migrate/20230607204102_create_statistics.rb
@@ -1,7 +1,8 @@
 class CreateStatistics < ActiveRecord::Migration[6.1]
   def change
     create_table :statistics do |t|
-      t.favorite_genre :total_games_played
+      t.integer :total_games_played
+      t.string :favorite_genre
       t.integer :hours_played
       t.references :user, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_06_07_204102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "game_logs", force: :cascade do |t|
+    t.integer "status"
+    t.integer "rating"
+    t.date "date_started"
+    t.date "date_stopped"
+    t.date "date_completed"
+    t.integer "play_time"
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_game_logs_on_game_id"
+    t.index ["user_id"], name: "index_game_logs_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.string "platform"
+    t.string "developers"
+    t.string "publishers"
+    t.date "release_date"
+    t.integer "players"
+    t.boolean "co_op"
+    t.string "overview"
+    t.string "genre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "avatar"
+    t.string "bio"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "statistics", force: :cascade do |t|
+    t.integer "total_games_played"
+    t.string "favorite_genre"
+    t.integer "hours_played"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_statistics_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "game_logs", "games"
+  add_foreign_key "game_logs", "users"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "statistics", "users"
 end
