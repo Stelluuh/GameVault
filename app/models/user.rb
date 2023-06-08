02a189292afd1096b2ae1c6614d44283577ff11f
stella <sftamesis13@gmail.com>
2023-06-08 09:33:00 -0700

    Add Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_one :profile
     has_many :game_logs
     has_many :games, through: :game_logs
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, true, length: { minimum: 8}
 end
