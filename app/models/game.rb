0cd320586aff03d00dc48ce5516ed6624b2952bb
stella <sftamesis13@gmail.com>
2023-06-09 14:00:43 -0700

    add associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,5 @@
 class Game < ApplicationRecord
-    has_many :game_logs
-    has_many :users, through: :game_logs
-
-    
-    
+  belongs_to :genre
+  has_many :game_logs
+  has_many :users, through: :game_logs
 end
