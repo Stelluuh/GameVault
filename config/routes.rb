801921cb326e3b97387045c2606a027cae50f526
stella <sftamesis13@gmail.com>
2023-06-12 13:59:20 -0700

    create routes for each model

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,15 @@ Rails.application.routes.draw do
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
 
+  resources :profiles, only: [:create, :show, :update, :destroy]
+  resources :games, only: [:index, :create, :show, :destroy]
+  resources :game_logs, only: [:show, :update, :destroy]
+  resources :genre, only: [:index]
+  
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+
 end
