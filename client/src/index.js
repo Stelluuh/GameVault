3119b827cdf463575b72f57673b6ae518dca7f69
stella <sftamesis13@gmail.com>
2023-06-12 14:21:11 -0700

    add reportWebVitals

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { reportWebVitals } from 'react-dom';
 import './index.css';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
