68f166ee08d01a78ae9e4700798e9afc3c9b00a4
stella <sftamesis13@gmail.com>
2023-06-12 14:08:17 -0700

    create and show methods defined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+    
+    def show
+        render json: @current_user
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id #logs in user
+        render json: user, status: :created
+    end
+
+    private
+
+    def find_user
+        user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation)
+    end
 end
